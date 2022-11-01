d2a0d6bad9dab72831f8cf33dda068246e37b307
Daiana <Daiana@DESKTOP-4N9BAGI>
2022-11-01 17:59:58 +0200

    lab4

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,12 @@
+﻿using Todean_Daiana_Lab2.Models;
+namespace Todean_Daiana_Lab2.ViewModels
+{
+    public class PublisherIndexData
+    {
+
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
+
