75601e2528670b143546885a0b5afa1522d4af54
Daiana <Daiana@DESKTOP-4N9BAGI>
2022-11-01 17:39:45 +0200

    lab3

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -45,7 +45,7 @@ namespace Todean_Daiana_Lab2.Pages.Books
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync(string[] selectedCategories)
         {
-            var newBook = new Book();
+            var newBook = Book;
             if (selectedCategories != null)
             {
                 newBook.BookCategories = new List<BookCategory>();
@@ -58,16 +58,20 @@ namespace Todean_Daiana_Lab2.Pages.Books
                     newBook.BookCategories.Add(catToAdd);
                 }
             }
-            if (await TryUpdateModelAsync<Book>(
-            newBook,
-            "Book",
-            i => i.Title, i => i.Author,
-            i => i.Price, i => i.PublishingDate, i => i.PublisherID))
-            {
-                _context.Book.Add(newBook);
-                await _context.SaveChangesAsync();
-                return RedirectToPage("./Index");
-            }
+            // if (await TryUpdateModelAsync<Book>(
+            // newBook,
+            // "Book",
+            //i => i.Title, i => i.Author,
+            // i => i.Price, i => i.PublishingDate, i => i.PublisherID))
+            //{
+            //    _context.Book.Add(newBook);
+            //    await _context.SaveChangesAsync();
+            //    return RedirectToPage("./Index");
+            // }
+
+            _context.Book.Add(newBook);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
             PopulateAssignedCategoryData(_context, newBook);
             return Page();
         }
